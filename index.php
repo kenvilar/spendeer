e957013d579a51131fdb84269804c90202baa256
Ken Vilar <kenvilar@gmail.com>
2017-12-09 03:23:38 +0800

    Add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/* Spendeer - A Very Simple Budget Application */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+
+
+<script src="main.js"></script>
+</body>
+</html>
