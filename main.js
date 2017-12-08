fcd842a40b4a81ccc851dbc312f4b24e931e8053
Ken Vilar <kenvilar@gmail.com>
2017-12-09 04:17:07 +0800

    Add new event listener for keypress

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-var budgetController = (function() {
+var budgetController = (function () {
 
 	// TODO
 
 })();
 
-var viewController = (function() {
+var viewController = (function () {
 
 	// TODO
 
 })();
 
-var appController = (function(budgetCtrl, viewCtrl) {
+var appController = (function (budgetCtrl, viewCtrl) {
 
-	document.querySelector('.add_button').addEventListener('click', function() {
+	document.querySelector('.add_button').addEventListener('click', function () {
 		// Get the input data
 		// Add data to budgetController
 		// Add data to viewController
 		// Calculate
 		// Display the result to the view
 		console.log('Button was clicked!');
+	});
+
+	document.addEventListener('keypress', function (e) {
+		if (e.keyCode === 13) {
+			console.log('You hit Enter key!');
+		} else {
+			console.log('Please hit Enter key!');
+		}
 	});
 
 })(budgetController, viewController);
