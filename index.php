31d559f8f9f35b0bc6a7c1ffd8fa1a1e9b9ada4d
Ken Vilar <kenvilar@gmail.com>
2017-12-09 04:38:41 +0800

    Add select type for income and expense; Add description and value input fields

--- index.php
+++ index.php
@@ -5,10 +5,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Title</title>
+    <meta charset="UTF-8">
+    <title>Title</title>
 </head>
 <body>
+
+<label for="">
+    <select name="item_type" class="item_type">
+        <option value="income">Income</option>
+        <option value="expense">Expense</option>
+    </select>
+</label>
+
+<label>
+    <input type="text" name="" class="item_description" placeholder="">
+</label>
+
+<label>
+    <input type="number" name="" class="item_value" placeholder="">
+</label>
 
 <button class="add_button">Add</button>
 
