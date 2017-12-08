e5b8e10933d0911a19c943613e11c2efeb9e3608
Ken Vilar <kenvilar@gmail.com>
2017-12-09 03:26:26 +0800

    Create appController to make the budgetController and viewController communicate to each other

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ var budgetController = (function() {
 var viewController = (function() {
 
 })();
+
+var appController = (function(budgetCtrl, viewCtrl) {
+
+	//
+
+})( budgetController, viewController );
