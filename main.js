ffa26ad70ab3eca3094d9ec8360f0e047b7c2f60
Ken Vilar <kenvilar@gmail.com>
2017-12-09 03:48:56 +0800

    Setup eventlistener for add button and add a log test

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 var budgetController = (function() {
 
+	// TODO
+
 })();
 
 var viewController = (function() {
+
+	// TODO
 
 })();
 
 var appController = (function(budgetCtrl, viewCtrl) {
 
-	//
+	document.querySelector('.add_button').addEventListener('click', function() {
+		console.log('Button was clicked!');
+	});
 
-})( budgetController, viewController );
+})(budgetController, viewController);
