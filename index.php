826cd539931b86b0370509a4295bf0704c7d27b4
Ken Vilar <kenvilar@gmail.com>
2017-12-09 21:41:12 +0800

    Add top section with background image

--- index.php
+++ index.php
@@ -6,9 +6,42 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:100,300,400,600" rel="stylesheet" type="text/css">
+    <link href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <title>Spendeer - A Very Simple Budget Application</title>
 </head>
 <body>
+
+<div class="wrapper">
+
+    <div class="top">
+        <div class="spendeer">
+            <div class="spendeer_title">
+                Available Budget in <span class="spendeer_title--month">December 2017</span>:
+            </div>
+
+            <div class="spendeer_value">- 0.00</div>
+
+            <div class="spendeer_income clearfix">
+                <div class="spendeer_income--text">Income</div>
+                <div class="right">
+                    <div class="spendeer_income--value">+ 0.00</div>
+                    <div class="spendeer_income--percentage">&nbsp;</div>
+                </div>
+            </div>
+
+            <div class="spendeer_expenses clearfix">
+                <div class="spendeer_expenses--text">Expenses</div>
+                <div class="right clearfix">
+                    <div class="spendeer_expenses--value">- 0.00</div>
+                    <div class="spendeer_expenses--percentage">---</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 <label for="">
     <select name="item_type" class="item_type">
