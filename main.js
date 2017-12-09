f1fd09befc7f7e9542aa6baea0222af89454d329
Ken Vilar <kenvilar@gmail.com>
2017-12-09 16:41:47 +0800

    Add Expense and Income function constructors

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var budgetController = (function () {
 
-	// TODO
+	var Expense = function (id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var Income = function (id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
 })();
 
